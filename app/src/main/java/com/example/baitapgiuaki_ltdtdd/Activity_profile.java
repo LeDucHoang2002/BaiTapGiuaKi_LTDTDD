5c9cf01c38c18db4372708c8cafe05e7087b2490
LeDucHoang2002 <leduchoang.10022002@gmail.com>
2022-10-06 10:43:49 +0700

    làm Profile kết hợp navigation bar

--- app/src/main/java/com/example/baitapgiuaki_ltdtdd/Activity_profile.java
+++ app/src/main/java/com/example/baitapgiuaki_ltdtdd/Activity_profile.java
@@ -1,14 +1,53 @@
 package com.example.baitapgiuaki_ltdtdd;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Activity_profile extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+import java.sql.RowId;
+
+    public class Activity_profile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+        private DrawerLayout drawerLayout;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_profile);
+            Toolbar toolbar=findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            drawerLayout=findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawr_open,R.string.navigation_drawr_close);
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView=findViewById(R.id.navigation_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id=item.getItemId();
+            if(id== R.id.nav_home){
+                Intent intent=new Intent(getApplicationContext(),Activity_Listview.class);
+                startActivity(intent);
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
+        @Override
+        public void onBackPressed() {
+            if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }else {
+                super.onBackPressed();
+            }
+        }
     }
-}
